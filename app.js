98cc8816cadafc92da539272df7680348c0568fb
mnshrm <asysnsrs665@gmail.com>
2023-10-10 16:04:18 +0530

    debug app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use("/", express.static("public"));
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started at port 5000");
 });
 
